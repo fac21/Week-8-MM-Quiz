e252c6fc8281c51e1878ab3426ab04ecd7752079
mikailbadoula <michaeldevfay@gmail.com>
2021-05-13 17:02:17 +0100

    Add score display

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,18 @@ import GameOver from './components/GameOver';
 function App() {
 	const [username, setUser] = React.useState('');
 	const [quizState, setQuizState] = React.useState('notStarted');
+	const [score, setScore] = React.useState(0);
 
 	return (
 		<div className='App'>
 			<div>
-				{quizState === "notStarted" ? <StartQuiz username={username} setUser={setUser} setQuizState={setQuizState} /> :
-				quizState === "started" ? <QuizData username={username} setQuizState={setQuizState}  /> :
-				<GameOver/>
-
-				}
+				{quizState === 'notStarted' ? (
+					<StartQuiz username={username} setUser={setUser} setQuizState={setQuizState} />
+				) : quizState === 'started' ? (
+					<QuizData username={username} setQuizState={setQuizState} score={score} setScore={setScore} />
+				) : (
+					<GameOver setQuizState={setQuizState} score={score} />
+				)}
 			</div>
 		</div>
 	);
--- src/components/QuizData.jsx
+++ src/components/QuizData.jsx
@@ -1,66 +1,64 @@
 import React from 'react';
-import Timer from "./Timer";
+import Timer from './Timer';
 function QuizData(props) {
 	const [quizData, setQuiz] = React.useState('');
 	const [index, setIndex] = React.useState(0);
-    const [timeLeft, setTimeLeft] = React.useState(20);
+	const [timeLeft, setTimeLeft] = React.useState(20);
 
 	React.useEffect(() => {
 		fetch('https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=boolean')
 			.then((res) => res.json())
 			.then((data) => setQuiz(data));
-				// setQuiz(data), console.log(data);
+		// setQuiz(data), console.log(data);
 	}, []);
 
-
-//if fetch is pending 
+	//if fetch is pending
 	if (!quizData) {
 		return <div>Loading...</div>;
-	} 
-
-//after resolved fetch
-const currentQuestion = quizData.results[index];
-const question = currentQuestion.question;
-const correct_answer = currentQuestion.correct_answer;
-const incorrect_answers = currentQuestion.incorrect_answers;
-const choices = [correct_answer, ...incorrect_answers];
-
-
-var shuffledChoices = choices.sort(() => Math.random() - 0.5);
-
-function checkAnswer(event){
-	if (event.target.textContent === correct_answer) {
-		setTimeLeft(timeLeft + 5);
-		// props.setScore(props.score + 1)
-		
-	}else{
-		setTimeLeft(timeLeft - 2);
 	}
-}
 
-return (
-	<main className="quiz">
-		<Timer timeLeft={timeLeft} setTimeLeft={setTimeLeft} setQuizState={props.setQuizState}/>
-		{ index === 0 ? <h1>Hi {props.username}!</h1> : null }
-		{ index < 9 ? <h2>{question}</h2> : 
-		props.setQuizState("gameOver")
+	//after resolved fetch
+	const currentQuestion = quizData.results[index];
+	const question = currentQuestion.question.replace(/&quot;/g, '"').replace(/&#039;/g, "'");
+	const correct_answer = currentQuestion.correct_answer;
+	const incorrect_answers = currentQuestion.incorrect_answers;
+	const choices = [correct_answer, ...incorrect_answers];
+
+	// var shuffledChoices = choices.sort(() => Math.random() - 0.5);
+
+	function checkAnswer(event) {
+		if (event.target.textContent === correct_answer) {
+			setTimeLeft(timeLeft + 5);
+			props.setScore(props.score + 1);
+		} else {
+			setTimeLeft(timeLeft - 2);
 		}
+	}
 
+	return (
+		<main className='quiz'>
+			<Timer timeLeft={timeLeft} setTimeLeft={setTimeLeft} setQuizState={props.setQuizState} />
+			{index === 0 ? <h1>Hi {props.username}!</h1> : null}
+			{index < 9 ? <h2>{question}</h2> : props.setQuizState('gameOver')}
 
-		<button onClick ={(event) =>{
-				checkAnswer(event)
-				setIndex(index + 1)
-			}
-			
-			} >{choices[0]}</button>
-		<button onClick ={(event) =>{
-				checkAnswer(event)
-				setIndex(index + 1)
-			}
-			
-			}>{choices[1]}</button>
-		
-		{/* {
+			<button
+				onClick={(event) => {
+					checkAnswer(event);
+					setIndex(index + 1);
+				}}
+			>
+				{choices[0]}
+			</button>
+			<button
+				onClick={(event) => {
+					checkAnswer(event);
+					setIndex(index + 1);
+				}}
+			>
+				{choices[1]}
+			</button>
+
+			{/* {
 		
 		
 		shuffledChoices.map(choice => (
@@ -74,10 +72,9 @@ return (
 		))
 		
 		} */}
-		 {/* <h2><span>SCORE:{props.score}</span></h2> */}
-	</main>
-)
-
-} 
+			{/* <h2><span>SCORE:{props.score}</span></h2> */}
+		</main>
+	);
+}
 
 export default QuizData;
